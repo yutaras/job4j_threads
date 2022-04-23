b43eb7e940f33b9f5fd005d82abec81a76e70b1d
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-23 17:27:10 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println(first.getState());
+        first.start();
+        System.out.println(second.getState());
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+            System.out.println(second.getState());
+        }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        System.out.println("Работа завершена");
+    }
+}
