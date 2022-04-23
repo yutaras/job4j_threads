352b619e291d8d6f6e3d2f0c27dc1db3822cfa6f
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-23 23:28:40 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 100; i++) {
+                            System.out.print("\rLoading : " + i + "%");
+                            Thread.sleep(1000);
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
