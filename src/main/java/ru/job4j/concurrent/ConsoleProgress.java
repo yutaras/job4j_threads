d582640bfca7905307c256993328e748ea14b7b7
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-29 10:50:39 +0300

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -26,7 +26,6 @@ public class ConsoleProgress implements Runnable {
                     Thread.sleep(500);
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
                 Thread.currentThread().interrupt();
             }
         }
