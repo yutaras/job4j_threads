4fbeb043e1adea3683b3af25d5fd5f5567e9e7ec
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-29 10:20:04 +0300

    3. Прерывание нити [#1019]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,31 +1,41 @@
 package ru.job4j.concurrent;
 
-import java.util.ArrayList;
+/**
+ * Класс демонстрирует пример прерывания нити, метод interrupt() выставляет флаг прерывания,
+ * но ни каких действий не совершает, для проверки флага воспользуемся методом isInterrupted().
+ * Метод Thread.interrupt() не выставляет флаг прерывания, если нить находится в режиме WAIT, JOIN.
+ * В этом случае методы sleep(), join(), wait() выкинут исключение.
+ * Поэтому нужно дополнительно проставить флаг прерывания.
+ * В блоке catch нужно дополнительно вызывать прерывание нити для того чтобы прерывание действительно произошло.
+ * Эта схема является шаблоном. Запомните, если используются методы sleep(), join() или wait(),
+ * то нужно в блоке catch вызвать прерывание.
+ *
+ * @author Tarasenko Yury
+ * @version 2.0
+ */
 
 public class ConsoleProgress implements Runnable {
 
     @Override
     public void run() {
-        String[] arr = new String[]{"\\", "|", "/"};
+        String[] ball = new String[]{"\\", "|", "/"};
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                for (String s : arr) {
+                for (String element : ball) {
+                    System.out.print("\r load: " + element);
                     Thread.sleep(500);
-                    System.out.print("\r load: " + s);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
-        Thread.currentThread().interrupt();
     }
 
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        Thread.sleep(10000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
         progress.interrupt();
     }
-
-
 }
