27b933dd3c17aee5cda4e74bd7d434db713fa552
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-30 17:00:41 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ShareNotSafe.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class ShareNotSafe {
+    public static void main(String[] args) throws InterruptedException {
+        UserCache cache = new UserCache();
+        User user = User.of("name");
+        cache.add(user);
+        Thread first = new Thread(
+                () -> {
+                    user.setName("rename");
+                }
+        );
+        first.start();
+        first.join();
+        System.out.println(cache.findById(1).getName());
+    }
+}
---
+++ src/main/java/ru/job4j/UserCache.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        ArrayList<User> copyUsers = new ArrayList<>();
+        for (User user : users.values()) {
+            copyUsers.add(User.of(user.getName()));
+        }
+        return copyUsers;
+    }
+}
