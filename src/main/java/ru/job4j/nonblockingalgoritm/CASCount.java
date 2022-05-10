1d6894d425e0015d313e4fb00880ee478cf48734
Yuriy Tarasenko <maxjeff@mail.ru>
2022-05-10 17:16:00 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/nonblockingalgoritm/CASCount.java
@@ -0,0 +1,22 @@
+package ru.job4j.nonblockingalgoritm;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+
+    public void increment() {
+        int value;
+        do {
+            value = count.get();
+        } while (!count.compareAndSet(value, value + 1));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
