932bcacae73f87c96140288b1df42bce623749bc
Yuriy Tarasenko <maxjeff@mail.ru>
2022-05-16 21:30:12 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pools/EmailNotification.java
@@ -0,0 +1,33 @@
+package ru.job4j.pools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = String.format("Notification %s to email %s",
+                    user.getUsername(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getEmail());
+            send(subject, body, user.getEmail());
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+}
